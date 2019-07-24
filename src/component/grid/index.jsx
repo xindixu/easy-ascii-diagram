636447c79cf3dd80976b50c53577c4c148e4a302
Xindi Xu <xindixu@utexas.edu>
2019-07-23 23:00:49 -0500

    primitive cal mouse position

--- src/component/grid/index.jsx
+++ src/component/grid/index.jsx
@@ -3,24 +3,31 @@ import PropTypes from 'prop-types';
 import { GRID_HEIGHT, GRID_WIDTH } from '../../constants';
 import { GridBackground } from './style';
 
-
 class Grid extends Component {
-  state = {
-    zoomLevel: 1,
-  }
-
   render() {
-    const { zoomLevel } = this.state;
-    const { onMouseDown, onMouseMove, onMouseUp } = this.props;
+    const {
+      zoomLevel, onMouseDown, onMouseMove, onMouseUp,
+    } = this.props;
+
     return (
-      <GridBackground width={GRID_WIDTH / zoomLevel} height={GRID_HEIGHT / zoomLevel} onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp} />
+      <GridBackground
+        width={GRID_WIDTH / zoomLevel}
+        height={GRID_HEIGHT / zoomLevel}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+      />
     );
   }
 }
 
+Grid.defaultProps = {
+  zoomLevel: 1,
+};
 Grid.propTypes = {
   onMouseDown: PropTypes.func.isRequired,
   onMouseMove: PropTypes.func.isRequired,
   onMouseUp: PropTypes.func.isRequired,
+  zoomLevel: PropTypes.number,
 };
 export default Grid;
--- src/container/sketch-pad/index.jsx
+++ src/container/sketch-pad/index.jsx
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { GRID_WIDTH, GRID_HEIGHT } from '../../constants';
 import Grid from '../../component/grid';
 import ToolBar from '../toolbar';
+
 
 const userStrokeStyle = '#EE92C2';
 const guestStrokeStyle = '#F0C987';
 
 class SketchPad extends Component {
-    state = {
-      isDrawing: false,
-      content: [],
-      tool: '',
-      prevPos: { x: null, y: null },
-      curPos: { x: null, y: null },
-    };
+  state = {
+    zoomLevel: 1,
+    isDrawing: false,
+    content: [],
+    tool: '',
+    prevPos: { x: null, y: null },
+    curPos: { x: null, y: null },
+  };
 
 
   handleMouseDown = (e) => {
     this.setState({
       isDrawing: true,
       prevPos: { x: e.clientX, y: e.clientY },
+      curPos: { x: e.clientX, y: e.clientY },
     });
   }
 
   handleMouseMove = (e) => {
-    this.setState({
-      curPos: { x: e.clientX, y: e.clientY },
-    });
+    if (this.state.isDrawing === true) {
+      this.setState({
+        curPos: { x: e.clientX, y: e.clientY },
+      });
+
+      this.draw();
+    }
   }
 
   handleMouseUp = (e) => {
@@ -35,6 +43,31 @@ class SketchPad extends Component {
     });
   }
 
+
+  calculateGridToFill() {
+    const { curPos, zoomLevel } = this.state;
+    const { x, y } = curPos;
+
+    const column = x / GRID_WIDTH * zoomLevel;
+    const row = y / GRID_HEIGHT * zoomLevel;
+    console.log(column, row);
+    const { _, totalColumn } = this.calculateTotalGridNumber();
+    const index = row * totalColumn + column;
+    return index;
+  }
+
+  calculateTotalGridNumber() {
+    const { zoomLevel } = this.state;
+    const totalRow = window.height / GRID_HEIGHT * zoomLevel;
+    const totalColumn = window.width / GRID_WIDTH * zoomLevel;
+    return { totalRow, totalColumn };
+  }
+
+
+  draw() {
+    const index = this.calculateGridToFill();
+    console.log(index);
+  }
 
   render() {
     const { prevPos, curPos } = this.state;
