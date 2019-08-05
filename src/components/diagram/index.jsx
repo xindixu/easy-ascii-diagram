4d4ae3e333fff06f481d7b04c3bdac9a99c699ed
Xindi Xu <xindixu@utexas.edu>
2019-08-04 23:40:17 -0500

    better alignment

--- src/components/diagram/index.jsx
+++ src/components/diagram/index.jsx
@@ -11,6 +11,9 @@ class Diagram extends Component {
     return (
       <Wrapper>
         <Content x={1} y={2}>+------------------+</Content>
+        {[...Array(5).keys()].map(el => <Content x={1} y={3 + el}>|</Content>)}
+
+
         <Content x={2} y={10}>+------------------+</Content>
       </Wrapper>
     );
--- src/components/diagram/style.jsx
+++ src/components/diagram/style.jsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
-
+import { styleSettings } from '../../assets/styles/settings';
 import {
   GRID_WIDTH, GRID_HEIGHT, CANVAS_WIDTH, CANVAS_HEIGHT, TOOLBAR_HEIGHT,
 } from '../../constants';
 
+const { yellow } = styleSettings;
 export const Wrapper = styled.div`
   width: ${CANVAS_WIDTH}px;
   height: ${CANVAS_HEIGHT}px;
@@ -17,11 +18,12 @@ export const Content = styled.code`
   font-size: ${GRID_WIDTH * 1.2}px;
   line-height: ${GRID_HEIGHT}px;
   letter-spacing: 2.25px;
-  vertical-align: text-top
+  vertical-align: text-top;
+  background: ${yellow};
   margin: 0;
   padding: 0;
   position: absolute;
-  ${props => `left: ${props.x * GRID_WIDTH + GRID_WIDTH / 5}px; 
+  ${props => `left: ${props.x * GRID_WIDTH}px; 
     top:${props.y * GRID_HEIGHT}px; `
 }
 `;
